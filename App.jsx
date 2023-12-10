bd4c8399b06e60a9a1a78ef34beb017c11949435
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2023-12-10 03:39:06 -0500

    Challenge complete

--- App.jsx
+++ App.jsx
@@ -10,8 +10,7 @@ import Weather from "./components/Weather"
 import ConfigMenu from "./components/ConfigMenu"
 
 export default function App() {
-    
-/* Challenge
+  /* Challenge
 
     The save button doesn't actually save the user's widget configuration. Your task is to fix this as follows: 
 
@@ -25,24 +24,22 @@ export default function App() {
               
     Bonus challenge: Complete the second task by using lazy state initialization. 
 */
-    
-	const DEFAULT_CONFIG = blankConfig.map(widget => {
-		return {...widget, positionData: {...widget.positionData}}
-	})
 
-	const [widgetConfig, setWidgetConfig] = React.useState(DEFAULT_CONFIG)
-	const [saveRequested, setSaveRequested] = React.useState(false)
+  const DEFAULT_CONFIG = blankConfig.map((widget) => {
+    return { ...widget, positionData: { ...widget.positionData } }
+  })
 
-    function save() {
-		setSaveRequested(true) // This causes the green "Saved!" message to be rendered on line 130 below. The state then gets set back to false by the setTimeout on line 70, which removes the message. 
-	} 
+  const [widgetConfig, setWidgetConfig] = React.useState(
+    JSON.parse(localStorage.getItem("widgetConfig")) || DEFAULT_CONFIG
+  )
+  const [saveRequested, setSaveRequested] = React.useState(false)
 
+  function save() {
+    localStorage.setItem("widgetConfig", JSON.stringify(widgetConfig))
+    setSaveRequested(true) // This causes the green "Saved!" message to be rendered on line 130 below. The state then gets set back to false by the setTimeout on line 70, which removes the message.
+  }
 
-
-
-
-
-/****** Write your code above! *******************************************************************  
+  /****** Write your code above! *******************************************************************  
  
  All of the code relevant to solving the challenge is above! Nothing about this project needs to be changed or analyzed outside of this code! 
     
@@ -50,90 +47,90 @@ export default function App() {
 
 ***************************************************************************************************/
 
-	const widgetComponents = {
-		Clock: <Clock />,
-		News: <News />,
-		Quote: <Quote />,
-		Stocks: <Stocks />,
-		ToDo: <ToDo />,
-		Weather: <Weather />
-	}
-    
-    const savedMessage = (
-        <div className="saved-message-container">
-            <p className="saved-message">Saved!</p>
-        </div>
-    )
-	
-	React.useEffect(()=>{
-		if (saveRequested) {
-			setTimeout(()=>{
-				setSaveRequested(false)	
-			}, 1000)
-		}
-	}, [saveRequested])
+  const widgetComponents = {
+    Clock: <Clock />,
+    News: <News />,
+    Quote: <Quote />,
+    Stocks: <Stocks />,
+    ToDo: <ToDo />,
+    Weather: <Weather />,
+  }
 
-	function dragHandler(e, data) {
-		if (e.target.dataset.type !== "button") {
-			setWidgetConfig((prevConfig) => {
-				let allConfigs = [...prevConfig]
-				let targetConfig = allConfigs.find(
-					(widget) => widget.name === data.node.classList[1]
-				)
-				targetConfig.positionData = {
-					...targetConfig.positionData,
-					customPosition: true,
-					x: data.x,
-					y: data.y,
-				}
-				return allConfigs
-			})
-		}
-	}
+  const savedMessage = (
+    <div className="saved-message-container">
+      <p className="saved-message">Saved!</p>
+    </div>
+  )
 
-	function getOffset(name) {
-		let targetConfig = widgetConfig.find((widget) => widget.name === name)
-		if (!targetConfig.positionData.customPosition) {
-			return undefined
-		} else {
-			return { x: targetConfig.positionData.x, y: targetConfig.positionData.y }
-		}
-	}
+  React.useEffect(() => {
+    if (saveRequested) {
+      setTimeout(() => {
+        setSaveRequested(false)
+      }, 1000)
+    }
+  }, [saveRequested])
 
-	const widgetsToDisplay = widgetConfig
-		.filter((widget) => widget.selected)
-		.map((widget) => {
-			const component = { ...widgetComponents[widget.name], key: nanoid() }
-			component.props = {
-				...component.props,
-				name: widget.name,
-				gridArea: widget.positionData.gridArea,
-				getOffset: getOffset,
-				dragHandler: dragHandler,
-				changeHandler: changeHandler,
-			}
-			return component
-		})
+  function dragHandler(e, data) {
+    if (e.target.dataset.type !== "button") {
+      setWidgetConfig((prevConfig) => {
+        let allConfigs = [...prevConfig]
+        let targetConfig = allConfigs.find(
+          (widget) => widget.name === data.node.classList[1]
+        )
+        targetConfig.positionData = {
+          ...targetConfig.positionData,
+          customPosition: true,
+          x: data.x,
+          y: data.y,
+        }
+        return allConfigs
+      })
+    }
+  }
 
-	function changeHandler(event) {
-		setWidgetConfig((prevConfig) => {
-			return prevConfig.map((widget) => {
-				return widget.name === event.target.name
-					? { ...widget, selected: !widget.selected }
-					: { ...widget }
-			})
-		})
-	}
+  function getOffset(name) {
+    let targetConfig = widgetConfig.find((widget) => widget.name === name)
+    if (!targetConfig.positionData.customPosition) {
+      return undefined
+    } else {
+      return { x: targetConfig.positionData.x, y: targetConfig.positionData.y }
+    }
+  }
 
-	return (
-		<div className="wrapper">
-            {saveRequested && savedMessage}
-			<div className="widget-container">{widgetsToDisplay}</div>
-			<ConfigMenu
-				stateProps={{ widgetConfig, setWidgetConfig, DEFAULT_CONFIG }}
-				changeHandler={changeHandler}
-				save={save}
-			/>
-		</div>
-	)
+  const widgetsToDisplay = widgetConfig
+    .filter((widget) => widget.selected)
+    .map((widget) => {
+      const component = { ...widgetComponents[widget.name], key: nanoid() }
+      component.props = {
+        ...component.props,
+        name: widget.name,
+        gridArea: widget.positionData.gridArea,
+        getOffset: getOffset,
+        dragHandler: dragHandler,
+        changeHandler: changeHandler,
+      }
+      return component
+    })
+
+  function changeHandler(event) {
+    setWidgetConfig((prevConfig) => {
+      return prevConfig.map((widget) => {
+        return widget.name === event.target.name
+          ? { ...widget, selected: !widget.selected }
+          : { ...widget }
+      })
+    })
+  }
+
+  return (
+    <div className="wrapper">
+      {saveRequested && savedMessage}
+      <div className="widget-container">{widgetsToDisplay}</div>
+      <ConfigMenu
+        stateProps={{ widgetConfig, setWidgetConfig, DEFAULT_CONFIG }}
+        changeHandler={changeHandler}
+        save={save}
+      />
+    </div>
+  )
 }
